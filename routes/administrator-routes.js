0ab7a8132b2bb66da8c2a5d0042c158c2db6e3ae
dilshad <dilshdadazamzam786@gmail.com>
2022-01-09 12:40:11 +0530

    routes-done

--- routes/administrator-routes.js
+++ routes/administrator-routes.js
@@ -4,15 +4,48 @@ import { body } from "express-validator";
 
 const router = express.Router();
 
-
-import { createEmployee} from "../controllers/create-employee.js";
+import { createEmployee } from "../controllers/create-employee.js";
 import { createDepartment } from "../controllers/create-department.js";
 
+// CREATE Employee
+router.post("/addemployee", [
+  body("name").not().isEmpty().trim().escape().withMessage("name is required"),
+  body("email")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("email is required"),
+  body("address")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("address is required"),
+  createEmployee,
+  body("contact_no")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("contact_no is required"),
+]);
 
-
-
-
-
-
+// CREATE department
+router.post("/adddepartment", [
+  body("d_name")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("d_name is required"),
+  body("employeeId")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("employeeiD is required"),
+  createDepartment,
+]);
 
 export default router;
